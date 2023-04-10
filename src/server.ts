34ceccbe008ce1db0c079023d43972b99378ec76
rachidb5 <jordan.rachid@gmail.com>
2023-04-10 00:23:47 -0300

    muda porta para facilitar o deploy

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,32 @@ import swaggerUi from 'swagger-ui-express'
 import * as swaggerDocument from './swagger.json'
 import * as bodyParser from 'body-parser'
 import router from './routes'
+const cors = require('cors');
 
 class App {
   private server: any
+  
+  private corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE'
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+      'authorization'
+    ],
+  };
 
   constructor() {
     this.server = express()
     this.server.use(bodyParser.urlencoded({ extended: true }));
     this.server.use(bodyParser.json());
+    this.server.use(cors(this.corsOpts));
     this.server.use(router)
     this.server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));     
   }
